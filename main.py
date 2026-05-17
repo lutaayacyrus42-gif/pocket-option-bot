22472b19e10a7c59e6560bcd6eae562fbe645919
lutaayacyrus42-gif <lutaayacyrus42@gmail.com>
2026-05-18 00:51:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ print(Fore.CYAN + "===================================")
 # SETTINGS
 # ====================================
 
-API_KEY = "1fc436c171bb47b2b714a9f4c1fe45a7"
+API_KEY = "1fc436c171bb47b2b714a9f4c1fe45a"
 
 TRADE_DURATION = 5
 MIN_CONFIDENCE = 80
